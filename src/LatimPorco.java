2335443cd37fb10b1a598b1ba472e547118b535f
joao <joao182cx@gmail.com>
2019-02-19 09:44:10 -0300

    Criação do método imprimeLatimPorco(), que traduz a frase inserida pelo usuário e depois a exibe na tela.

--- src/LatimPorco.java
+++ src/LatimPorco.java
@@ -5,22 +5,58 @@ public class LatimPorco {
 	public static void main(String[] args) {
 		
 		String frase;
-		String fraseLatimPorco = "";
+		//String fraseLatimPorco = "";
+		String[] tokens;
 		
 		Scanner input = new Scanner(System.in);
 		
 		System.out.println("Insira a sua frase:");
 		frase = input.nextLine();
+		frase = frase.toLowerCase();
+		if(frase.contains(".")) {
+			frase.replace(".", "");
+		}
 		
-		fraseLatimPorco += frase.split(" ");
-		System.out.println(fraseLatimPorco);
+
+		imprimeLatimPorco(frase);	
+
 		
 	}
 	
-	public static String traduzLatimPorco(String a){
+	public static void imprimeLatimPorco(String a){
 		
-		return a.substring(1) + a.charAt(0) + "ay";
+		String[] tokens = a.split(" ");
+		
+		for(int i = 0; i < tokens.length; i++) {
+			
+			//System.out.printf(tokens[i].substring(1) + tokens[i].charAt(0) + "ay");
+			
+			if(i == 0) {
+				
+				System.out.printf((tokens[i].substring(1) + tokens[i].charAt(0) + "ay").substring(0, 1).toUpperCase() +
+						(tokens[i].substring(2) + tokens[i].charAt(0) + "ay"));
+				
+			}else {
+				
+				System.out.printf(tokens[i].substring(1) + tokens[i].charAt(0) + "ay");
+				
+			}
+			
+			if(i == tokens.length - 1) {
+				
+				System.out.printf(".");
+				
+			}else {
+				
+				System.out.printf(" ");
+				
+			}
+			
+		}
+		
 		
 	}
 
 }
+
+
